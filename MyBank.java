1b228d29c90603e4630a15695da26ed1f67537c3
Ginucg <99877666+Ginucg@users.noreply.github.com>
2022-04-24 09:56:42 +0530

    Add files via upload

---
+++ MyBank.java
@@ -0,0 +1,18 @@
+package p1;
+
+final  class MyBank {
+	final int accno=761932;
+	final void modify(){
+		 // accno=accno+6;//error
+		System.out.println("modify");
+	}
+	//public class MyClassFinDemo extends mybank {  final class cannot have subclasses
+	//void display(){ Error final method cannot be  overridan
+	//System.out.println("override modify");//
+//      }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
+
